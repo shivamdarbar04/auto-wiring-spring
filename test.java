62e2daf67b04b2350652ba3a0f68b6166e657d1c
Shivam Singh Darbar <shivamdarbar04@gmail.com>
2023-07-16 18:23:16 +0530

    auto-wiring-spring

---
+++ test.java
@@ -0,0 +1,15 @@
+package com.springcore.auto.wire.annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test 
+{
+
+	public static void main(String[] args) 
+	{
+	 ApplicationContext context =  new ClassPathXmlApplicationContext("com/springcore/auto/wire/annotations/aotoconfig.xml");	
+	 Emp emp =  context.getBean("emp1",Emp.class);
+	 System.out.println(emp);
+	}
+}
